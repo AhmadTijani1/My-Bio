90d7a8d9536e7b54fb3fd281a7272dc3b536c763
AhmadTijani1 <129990920+AhmadTijani1@users.noreply.github.com>
2023-05-12 00:38:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btn');
+const myStack = document.getElementById('my-stack');
+btn.addEventListener('click',() => { 
+    myStack.textContent= 'I am currently learning JavaScript';
+});
